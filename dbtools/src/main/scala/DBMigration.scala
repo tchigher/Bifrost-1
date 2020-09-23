7cac605c455cb8e980ce5396e3195f8ad9d470cf
jingsting <jingfei1024@gmail.com>
2020-09-23 13:13:30 -0500

    Append block to history and applyModifier to state

--- dbtools/src/main/scala/DBMigration.scala
+++ dbtools/src/main/scala/DBMigration.scala
@@ -8,23 +8,23 @@ import scorex.crypto.encode.Base58
 import io.iohk.iodb.ByteArrayWrapper
 
 import scala.collection.mutable.ArrayBuffer
+import scala.reflect.io.Path
+import scala.util.Try
 
 object DBMigration extends Logging {
 
   private def migrate(oldSettings: AppSettings, newSettings: AppSettings): Unit = {
     var listBlockId: ArrayBuffer[Array[Byte]] = ArrayBuffer[Array[Byte]]()
     val oldHistory: History = History.readOrGenerate(oldSettings)
-    val newHistory: History = History.readOrGenerate(newSettings)
-//    val newState: State = State.readOrGenerate(newSettings, callFromGenesis = true, newHistory)
+    var newHistory: History = History.readOrGenerate(newSettings)
+    var newState: State = State.readOrGenerate(newSettings, callFromGenesis = true, newHistory)
     var blockId: ModifierId = oldHistory.bestBlockId
     var height: Long = oldHistory.height
     var start = System.nanoTime()
 
-    println(s"---------------${oldSettings.dataDir}")
-
     log.info(s"Blockchain height is ${height}, blockId: ${Base58.encode(blockId.hashBytes)}")
 
-    while(!(blockId.hashBytes sameElements History.GenesisParentId) && height > 1059465) {
+    while(!(blockId.hashBytes sameElements History.GenesisParentId)) {
       val currentBlock: Block = oldHistory.storage.storage.get(ByteArrayWrapper(blockId.hashBytes)).map { bw =>
         val bytes = bw.data
         BlockSerializer.decode(bytes.tail).get
@@ -37,22 +37,24 @@ object DBMigration extends Logging {
       height = height - 1
     }
 
-    val duration = (System.nanoTime() - start) / 1e9d
-
+    var duration = (System.nanoTime() - start) / 1e9d
     log.info(s"Found ${listBlockId.size} blockIds in $duration seconds")
 
     start = System.nanoTime()
 
-    for (bid <- listBlockId) {
+    height = 0
+    for (bid <- listBlockId.reverse) {
       val currentBlock: Block = oldHistory.storage.storage.get(ByteArrayWrapper(bid)).map { bw =>
         val bytes = bw.data
         BlockSerializer.decode(bytes.tail).get
-      }.get
-      println(s"-------${Base58.encode(bid)}-----${Base58.encode((currentBlock.serializedId))}")
-//      newHistory.append(currentBlock)
-//      state.applyModifier(currentBlock)
+      }.get.copy(parentId = newHistory.bestBlockId)
+      println(s"Height:$height----BlockId:${Base58.encode(bid)}----BlockSerializedId:${Base58.encode(currentBlock.serializedId)}----BlockParentId:${Base58.encode(currentBlock.parentId.hashBytes)}")
+      height = height + 1
+      newHistory = newHistory.append(currentBlock).get._1
+      newState = newState.applyModifier(currentBlock).get
     }
 
+    duration = (System.nanoTime() - start) / 1e9d
     log.info(s"Migrated ${listBlockId.size} blocks in $duration seconds")
   }
 
@@ -64,6 +66,10 @@ object DBMigration extends Logging {
 
     val newSettingsFilename = "dbtools/src/main/resources/newData.conf"
     val newSettings: AppSettings = AppSettings.read(StartupOpts(Some(newSettingsFilename), None))
+
+    /* If new-data data exists, remove before creating new-data */
+    val path: Path = Path(".bifrost/new-data")
+    Try(path.deleteRecursively())
 
     migrate(oldSettings, newSettings)
   }
